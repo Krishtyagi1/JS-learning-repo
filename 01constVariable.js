0f3815547a97deeef3218f51da0d6528c0926ca1
Krishtyagi1 <165552612+Krishtyagi1@users.noreply.github.com>
2026-06-26 18:51:41 +0530

    Create 01constVariable.js

---
+++ 01constVariable.js
@@ -0,0 +1,22 @@
+const accountId=1443  
+// const cannot be changed 
+let accountEmail="krish@gmail.com"
+var accountPass="121212"
+accountCity="sonipat"
+
+let accountState;
+// undefined value 
+
+accountEmail="krish22@gmail.com"
+accountPass="11111"
+
+console.log(accountState);
+
+console.table([accountEmail, accountPass, accountCity]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
